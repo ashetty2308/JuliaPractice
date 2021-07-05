6d9ca9e6498b8fd366915901f0b02ff253bcc861
Adit Shetty <ashetty2308@gmail.com>
2021-07-05 16:33:15 -0400

    Added parabola

--- Desktop/JuliaVSCode/main.jl
+++ Desktop/JuliaVSCode/main.jl
@@ -1,9 +1,13 @@
 using Plots
 
-#=
-Line Plot:
-x = 1:10; y = rand(10); # These are the plotting data
-plot(x,y, label="Julia Data")
-=#
 
+#Line Plot:
+x = 1:10; y = rand(10); 
+plot(x,y, label="Julia Data")
+
+
+#Parabola:
+f(x) = x^2+3
+x = 1:65
+scatter(x,f.(x))
 
